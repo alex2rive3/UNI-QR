a57db5e827063e1cdc1e3dabdc95cb535b4fe430
alex2rive3 <alex2rive3@gmail.com>
2023-03-06 19:57:02 -0300

    actualizando login

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,6 +1,7 @@
 const { User } = require("../model/user.model");
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
+//import Cookies from 'universal-cookie';
 module.exports.register = (req, res) => {
     const user = new User(req.body);
     user.save()
@@ -24,6 +25,7 @@ module.exports.logout = (req, res) => {
     }
 }
 module.exports.login = async (req, res) => {
+    //console.log(req.body)
     try {
         const { email, password } = req.body;
         const usuario = await User.findOne({ email: email })
@@ -35,12 +37,13 @@ module.exports.login = async (req, res) => {
             const secret = "clave sercret jwt"
             const newJWT = jwt.sign({
                 _id: usuario._id,
-                usuario: usuario.userName
+                usuario: usuario.userName,
+                acceso: usuario.permit
             }, secret)
             res.cookie("userToken", newJWT, {
                 httpOnly: true
             })
-            res.json({ msg: "Logueado correctamente " })
+            res.json({ msg: "Logueado Correctamente" })
 
         } else {
             return res.status(403).json({ msg: "Correo o contraseña Incorrecta" })
@@ -50,8 +53,22 @@ module.exports.login = async (req, res) => {
         res.json(error)
     }
 }
+
+module.exports.generador = async (req, res) => {
+    try {
+        const secret = "boletoDescuentoFram";
+        const newTokenQR = jwt.sign({
+            _id: new Date(),
+        }, secret)
+        res.json(newTokenQR);
+    } catch (error) {
+        return res.json({ msg: "Ha ocurrido algun error " })
+    }
+}
+
+
 module.exports.buscarEmail = async (req, res) => {
-    console.log(req.body)
+    //console.log(req.body)
     const { email } = req.body
     const result = await User.findOne({ email: email })
     res.json(result)
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,9 +3,10 @@ const { authenticate } = require('../config/jwt.config');
 module.exports = app => {
     app.post("/api/register", User.register);
     app.post("/api/login", User.login);
-    app.get("/api/user", authenticate, User.getAll);
+    //app.post("/api/generar", authenticate, User.generador);
+    app.post("/api/generar", User.generador);
     app.get("/api/libre", User.getAll);
     app.get("/api/salir", User.logout);
-    app.post("/api/buscar", User.buscarEmail);
+    app.post("/api/user", User.buscarEmail);
 }
 
